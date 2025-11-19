848571012afb1957c0528eeea6eae6b9458f199f
marvinomondi183 <130049692+marvinomondi183@users.noreply.github.com>
2025-11-19 12:39:39 +0300

    Add files via upload

---
+++ Assignment1/Stringlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char Str[50];
+    char Str[50];
+
+    printf("Enter the string: ");
+    scanf("%s",&Str);
+    printf("\nThe length of the string '%s' is %zu.",Str, strlen(Str));
+    return 0;
+}
